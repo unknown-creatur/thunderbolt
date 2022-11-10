8f2d3aeeedde16eef5c40fd9a4eee7abcd0cf882
421385 <115143096+unknown-creatur@users.noreply.github.com>
2022-11-10 16:43:19 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
-let x = 0
-let y = 0
-basic.forever(function () {
-    if (x == 4) {
-        for (let index = 0; index < 5; index++) {
-            basic.clearScreen()
-            led.plot(x, y)
-            x += -1
-            basic.pause(100)
-        }
-    } else {
-        x += 1
-        led.plot(x, y)
-        basic.pause(100)
-        basic.clearScreen()
+function brightest () {
+    for (let index = 0; index < 5; index++) {
+        led.plot(x1, y)
+        y += 1
     }
+}
+function brighter () {
+    for (let index = 0; index < 5; index++) {
+        led.plotBrightness(x2, y, 60)
+        y += 1
+    }
+}
+let y = 0
+let x2 = 0
+let x1 = 0
+x1 = 0
+x2 = 0
+y = 0
+let dx = 0
+basic.forever(function () {
+    basic.clearScreen()
+    y = 0
+    if (x1 == 0) {
+        dx = 1
+    } else if (x1 == 4) {
+        dx = -1
+    }
+    x1 += dx
+    brightest()
+    brighter()
+    basic.pause(100)
 })
