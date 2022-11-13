33a6380ec75e4df80bfdbb45be1e01517fb2b34a
421385 <115143096+unknown-creatur@users.noreply.github.com>
2022-11-13 20:58:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,13 +6,13 @@ function brightest () {
 }
 function bright () {
     for (let index = 0; index < 5; index++) {
-        led.plotBrightness(x3, y, 20)
+        led.plotBrightness(x2, y, 60)
         y += 1
     }
 }
 function brighter () {
     for (let index = 0; index < 5; index++) {
-        led.plotBrightness(x2, y, 60)
+        led.plotBrightness(x3, y, 20)
         y += 1
     }
 }
@@ -30,23 +30,32 @@ let dx3 = 1
 basic.forever(function () {
     basic.clearScreen()
     y = 0
-    if (x1 == 0) {
+    if (x1 <= 0) {
         dx1 = 1
-    } else if (x1 == 4) {
+        basic.pause(100)
+        dx2 = 1
+        basic.pause(100)
+        dx3 = 1
+    } else if (x1 >= 4) {
         dx1 = -1
-    }
-    if (x2 == 0) {
-        dx1 = 1
-    } else if (x1 == 4) {
-        dx1 = -1
+        basic.pause(100)
+        dx2 = -1
+        basic.pause(100)
+        dx3 = -1
     }
     x1 += dx1
+    basic.pause(100)
     x2 += dx2
+    basic.pause(100)
     x3 += dx3
-    for (let index = 0; index < 4; index++) {
-        brightest()
-        brighter()
-        bright()
+    for (let index = 0; index < 5; index++) {
+        led.plot(x1, y)
+        led.plotBrightness(x2, y, 60)
+        led.plotBrightness(x3, y, 20)
+        y += 1
     }
+    led.unplot(x2, y)
+    basic.pause(100)
+    led.unplot(x3, y)
     basic.pause(100)
 })
